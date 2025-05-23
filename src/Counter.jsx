8399cf2205dbc7246de0e619682f81759b2ead62
Nick536363 <134803463+Nick536363@users.noreply.github.com>
2025-05-23 15:13:59 +0200

    added counter in Counter.jsx

--- src/Counter.jsx
+++ src/Counter.jsx
@@ -1,7 +1,14 @@
+import "./Counter.css";
+
 function helloCounter(){
     return(
-        <h1>Hello Counter</h1>
+        <div class="Counter">
+            <div> <h2>Счётчик:</h2> <h1>0</h1>
+                <button class="minus">- Минус</button>
+                <button class="plus">+ Плюс</button>
+            </div>
+        </div>
     );
-}
+}   
 
 export default helloCounter;
