c7b63018516d1106823285bc3dd9c01ce5994305
Nick536363 <134803463+Nick536363@users.noreply.github.com>
2025-05-24 13:47:01 +0200

    added Add and Subsract functions for counter

--- src/Counter.jsx
+++ src/Counter.jsx
@@ -1,14 +1,22 @@
 import "./Counter.css";
+import { useState } from "react";
 
-function helloCounter(){
+function HelloCounter(){
+    const [Counter, setCount] = useState(0)
+    const Add = () => {
+        setCount(Counter+1)
+    }
+    const Substract = () => {
+        setCount(Counter-1)
+    }
     return(
         <div class="Counter">
-            <div> <h2>Счётчик:</h2> <h1>0</h1>
-                <button class="minus">- Минус</button>
-                <button class="plus">+ Плюс</button>
+            <div> <h2>Счётчик:</h2> <h1>{Counter}</h1>
+                <button class="minus" onClick={Substract}>- Минус</button>
+                <button class="plus" onClick={Add}>+ Плюс</button>
             </div>
         </div>
     );
 }   
 
-export default helloCounter;
+export default HelloCounter;
